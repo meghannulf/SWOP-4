77936485466b013cc52f3f0fb64f0f1c3eaaea00
CJ5518 <carsonrueber@gmail.com>
2023-11-06 12:03:21 -0800

    Initial commit

---
+++ query.py
@@ -0,0 +1,25 @@
+import requests
+import urllib.parse
+import argparse
+
+#Example usage:
+#python3 query.py -q "select distinct ?Concept where {[] a ?Concept} LIMIT 100"  -f "xml" -o "./out.txt"
+
+parser = argparse.ArgumentParser(description="Python utility to query dbpedia")
+parser.add_argument("--format", "-f", help="Format to receive results in", type=str, default="csv", choices=["xml","html","csv","json"])
+parser.add_argument("--query", "-q", help="Query to send to dbpedia", type=str, default=None, required=True)
+parser.add_argument("--outfile", "-o", help="Path to make the output file", type=str, default=None, required=True)
+
+args = parser.parse_args()
+
+
+def doQuery(query, format, outfile):
+	safeQueryString = urllib.parse.quote_plus(query)
+
+	res = requests.get("https://dbpedia.org/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=" + safeQueryString + "&format=text%2F" + format + "&timeout=30000")
+	file = open(outfile, "w")
+	file.write(res.content.decode("utf-8"))
+	file.close()
+
+
+doQuery(args.query, args.format, args.outfile)
